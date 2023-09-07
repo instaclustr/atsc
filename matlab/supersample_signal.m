f4f7bc853b72921374399addac0c672b0fb0c8b5
Carlos Rolo <3799585+cjrolo@users.noreply.github.com>
2023-09-07 17:38:21 +0100

    Function to adding data to signals

---
+++ matlab/supersample_signal.m
@@ -0,0 +1,23 @@
+function [x, y] = supersample_signal(ts, ts_sampling_rate, polinomial)
+  warning ('off', 'all') ;
+  tic
+  x = (1:ts_sampling_rate:ts_sampling_rate*length(ts));
+  window = polinomial * ts_sampling_rate;
+  y = [];
+  window_count = 0;
+  for i=1:polinomial:length(ts)
+    top = i+polinomial;
+
+    if top > length(x)
+      top = length(x);
+    endif
+    if i == top
+      break;
+    endif
+    yi = polyfit(x(i:top),ts(i:top),3);
+    yii = polyval(yi,window*window_count+1:window*(window_count+1)+1);
+    y = [y(1:end-1) yii];
+    window_count = window_count + 1;
+  end
+  toc
+  plot(y, 'r', x,ts, '+');
