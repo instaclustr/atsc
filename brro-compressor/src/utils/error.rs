33b81af64328e085d09026063ac29f0010dcc494
Carlos Rolo <3799585+cjrolo@users.noreply.github.com>
2023-09-27 16:31:32 +0100

    Adjustments on the calculate error return/tests

--- brro-compressor/src/utils/error.rs
+++ brro-compressor/src/utils/error.rs
@@ -10,16 +10,16 @@ use std::cmp;
 /// # Returns
 ///
 /// The mean squared error, or an error message if the vector lengths are different.
-fn calculate_error(vec1: &Vec<f64>, vec2: &Vec<f64>) -> Result<f64, &'static str> {
+fn calculate_error(vec1: &Vec<f64>, vec2: &Vec<f64>) -> Option<f64> {
     if vec1.len() != vec2.len() {
-        return Err("Vector lengths are not the same.");
+        return None;
     }
 
     let min_length = cmp::min(vec1.len(), vec2.len());
     let squared_error: f64 = (0..min_length)
         .map(|i| (vec1[i] - vec2[i]).powi(2))
         .sum();
-    Ok(squared_error / min_length as f64)
+    Some(squared_error / min_length as f64)
 }
 
 #[cfg(test)]
@@ -29,11 +29,11 @@ mod tests {
     #[test]
     fn test_calculate_error() {
         let vector1 = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-        let vector2 = vec![1.5, 2.5, 2.8, 3.7, 4.9];
+        let vector2 = vec![2.5, 4.0, 6.0, 8.0, 10.0];
+        let vector3 = vec![1.5, 2.5, 2.8, 3.7];
 
-        match calculate_error(&vector1, &vector2) {
-            Ok(error) => println!("The mean squared error between vector1 and vector2 is: {:.2}", error),
-            Err(msg) => panic!("Error: {}", msg),
-        }
+        assert_eq!(calculate_error(&vector1, &vector1), Some(0.0));
+        assert_eq!(calculate_error(&vector1, &vector2), Some(11.25));
+        assert_eq!(calculate_error(&vector1, &vector3), None);
     }
 }
