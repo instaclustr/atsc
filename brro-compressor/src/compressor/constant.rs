fb75df5001ffcdd3e0ca66781f083879f55b7800
Carlos Rolo <3799585+cjrolo@users.noreply.github.com>
2023-09-27 13:45:27 +0100

    implementation of noop and constant. Working?

--- brro-compressor/src/compressor/constant.rs
+++ brro-compressor/src/compressor/constant.rs
@@ -9,18 +9,20 @@ const CONSTANT_COMPRESSOR_ID: u8 = 0;
 pub struct Constant {
     pub id: u8,
     pub constant: i64,
-    pub error: f64,
+    // Lets make the error, the number of residuals/frame lenght
+    pub error: f32,
     pub residuals: Vec<(i32, i64)>,
 }
 
 impl Constant {
-    pub fn new() -> Self {
+    /// Creates a new instance of the Constant compressor with the size needed to handle the worst case
+    pub fn new(frame_size: usize) -> Self {
         debug!("Constant compressor");
         Constant {
             id: CONSTANT_COMPRESSOR_ID,
             constant: 0,
             error: 0.0,
-            residuals: Vec::new(),
+            residuals: Vec::with_capacity(frame_size),
         }
     }
 
@@ -29,11 +31,14 @@ impl Constant {
         self.constant = constant_value;
     }
 
-    /// Adds an error point to the error point vector.
-    /// It needs the sample number (where the error happened in the signal)
-    /// and the error value.
+    /// Adds a residual
     pub fn add_residual(&mut self, error_sample: i32, error_value: i64) {
         self.residuals.push((error_sample, error_value));
+    }
+
+    /// Sets the error value
+    fn set_error(&mut self, data_lenght: usize){
+        self.error = (self.residuals.len()/data_lenght) as f32;
     }
 
     /// Currently the data is provided in f64, this compressor needs i64. So the optimizer needs
@@ -45,6 +50,7 @@ impl Constant {
 
     /// Compresses the data. Walks the data array and sets one value as the constant.
     /// TODO: VERY INEFFICIENT, we walk the data, and then the resulting map 2x. Find opportunities to improve
+    /// TODO2: Fix residuals positions
     pub fn compress(&mut self, data: &[i64]) {
         // Count occurrences of each value in the data
         let mut seen_values = HashMap::new();
@@ -71,15 +77,23 @@ impl Constant {
 
         self.set_constant(constant);
         self.residuals = residuals;
+        self.set_error(data.len());
     }
 
     /// This function transforms the structure into a Binary stream to be appended to the frame
     pub fn to_bytes(self) -> Vec<u8> {
-        // Use Bincode and flate2-rs?
+        // Use Bincode and flate2-rs? Do this at the Stream Level?
         Vec::new()
     }
 }
 
 pub fn constant(data: &[f64]) -> Vec<u8> {
-    Vec::new()
+    info!("[Compressor] Initializing Constant Compressor");
+    // Initialize the compressor
+    let mut c = Constant::new(data.len());
+    // Convert the data via the optimizer
+    c.compress(&Constant::optimize(data));
+    // Convert to bytes
+    c.to_bytes()
 }
+// TODO: WRITE A TESTTTTTTTTTT!!!!!!!!!!!!!!
--- brro-compressor/src/compressor/noop.rs
+++ brro-compressor/src/compressor/noop.rs
@@ -1,15 +1,21 @@
 use log::{info, debug};
 
-const NOOP_COMPRESSOR_ID:u8 = 256;
+const NOOP_COMPRESSOR_ID:u8 = 255;
 
 pub struct Noop {
     pub id: u8,
+    data: Vec<f64>,
 }
 
 impl Noop {
-    pub fn new() -> Self {
+    pub fn new(frame_size: usize) -> Self {
         debug!("Noop compressor");
-        Noop { id: NOOP_COMPRESSOR_ID }
+        Noop { id: NOOP_COMPRESSOR_ID, data: Vec::with_capacity(frame_size) }
+    }
+
+    /// "Compress"
+    pub fn compress(&mut self, data: &[f64]) {
+        self.data = data.to_vec();
     }
 
     /// This function transforms the structure in a Binary stream to be appended to the frame
@@ -21,5 +27,8 @@ impl Noop {
 }
 
 pub fn noop(data: &[f64]) -> Vec<u8> {
-    Vec::new()
+    info!("[Compressor] Initializing Noop Compressor");
+    let mut c = Noop::new(data.len());
+    c.compress(data);
+    c.to_bytes()
  }
