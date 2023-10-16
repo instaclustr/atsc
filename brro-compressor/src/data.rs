e0771ea0a60e2da08e1265271d1714d17cca54ca
Carlos Rolo <3799585+cjrolo@users.noreply.github.com>
2023-10-16 14:44:24 +0100

    Test fix

--- brro-compressor/src/data.rs
+++ brro-compressor/src/data.rs
@@ -84,7 +84,7 @@ mod tests {
         let mut cs = CompressedStream::new();
         cs.compress_chunk_with(&vector1, Compressor::Constant);
         let b = cs.to_bytes();
-        assert_eq!(b, [66, 82, 82, 79, 0, 1, 37, 0, 3, 3, 0, 2, 0]);
+        assert_eq!(b, [66, 82, 82, 79, 0, 1, 41, 251, 0, 4, 3, 3, 0, 2, 0]);
     }
 
     #[test]
