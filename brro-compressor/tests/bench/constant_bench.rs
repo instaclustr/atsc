330983b6e5cb2874ee8ce3628c71d37ebf818d81
joshuabvarghese <joshualogin7@gmail.com>
2023-11-13 15:10:35 +1100

    Add SeedableRng

--- brro-compressor/tests/bench/constant_bench.rs
+++ brro-compressor/tests/bench/constant_bench.rs
@@ -1,11 +1,10 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use brro_compressor::compressor::constant::{constant, constant_to_data, Constant};
-use rand::Rng;
+use rand::{SeedableRng, Rng};
 
-/// Generates random data of a given size.
-fn generate_random_data(size: usize) -> Vec<f64> {
-    let thread_rng = rand::thread_rng();
-    let mut rng = thread_rng;
+/// Generates random data of a given size with a fixed seed.
+fn generate_random_data_with_seed(size: usize, seed: u64) -> Vec<f64> {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
     (0..size)
         .map(|_| rng.gen_range(0.0..1.0))
         .collect()
@@ -14,12 +13,13 @@ fn generate_random_data(size: usize) -> Vec<f64> {
 /// Benchmark for constant compression.
 fn constant_compression_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Constant Compression");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let input = Constant::optimize(&data);
 
         group.bench_function(format!("Compress (size={})", size), |b| {
@@ -37,12 +37,13 @@ fn constant_compression_benchmark(c: &mut Criterion) {
 /// Benchmark for constant decompression.
 fn constant_decompression_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Constant Decompression");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let compressed_data = constant(&data);
 
         group.bench_function(format!("Decompress (size={})", size), |b| {
@@ -59,12 +60,12 @@ fn constant_decompression_benchmark(c: &mut Criterion) {
 /// Benchmark for constant optimization.
 fn constant_optimization_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
-
+    let seed = 42;
     let mut group = c.benchmark_group("Constant Optimization");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
 
         group.bench_function(format!("Optimize (size={})", size), |b| {
             b.iter(|| {
@@ -79,12 +80,13 @@ fn constant_optimization_benchmark(c: &mut Criterion) {
 /// Benchmark for comparing compression and decompression.
 fn constant_compression_vs_decompression_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Compression vs. Decompression");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let input = Constant::optimize(&data);
         let compressed_data = constant(&data);
 
@@ -110,11 +112,12 @@ fn constant_compression_vs_decompression_benchmark(c: &mut Criterion) {
 /// Benchmark for compression ratio.
 fn constant_compression_ratio_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Constant Compression Ratio");
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let input = Constant::optimize(&data);
 
         group.bench_function(format!("Compression Ratio (size={})", size), |b| {
@@ -133,12 +136,13 @@ fn constant_compression_ratio_benchmark(c: &mut Criterion) {
 /// Benchmark for comparing compression and data size.
 fn constant_compression_vs_data_size_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Compression vs. Data Size");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let input = Constant::optimize(&data);
 
         group.bench_function(format!("Compress (size={})", size), |b| {
@@ -156,12 +160,13 @@ fn constant_compression_vs_data_size_benchmark(c: &mut Criterion) {
 /// Benchmark for comparing decompression and data size.
 fn constant_decompression_vs_data_size_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Decompression vs. Data Size");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let compressed_data = constant(&data);
 
         group.bench_function(format!("Decompress (size={})", size), |b| {
@@ -178,12 +183,13 @@ fn constant_decompression_vs_data_size_benchmark(c: &mut Criterion) {
 /// Benchmark for memory usage during compression and decompression.
 fn constant_memory_usage_benchmark(c: &mut Criterion) {
     let data_sizes = [100, 1000, 5000];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Memory Usage");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
-        let data = generate_random_data(*size);
+        let data = generate_random_data_with_seed(*size, seed);
         let input = Constant::optimize(&data);
 
         group.bench_function(format!("Compression Memory (size={})", size), |b| {
@@ -210,13 +216,14 @@ fn constant_memory_usage_benchmark(c: &mut Criterion) {
 fn constant_compression_vs_residuals_benchmark(c: &mut Criterion) {
     let data_sizes = [1000, 5000, 10000];
     let residuals_counts = [10, 50, 100];
+    let seed = 42;
 
     let mut group = c.benchmark_group("Compression vs. Residuals");
     group.throughput(Throughput::Elements(1));
 
     for size in data_sizes.iter() {
         for residuals_count in residuals_counts.iter() {
-            let mut data = generate_random_data(*size);
+            let mut data = generate_random_data_with_seed(*size, seed);
             data.resize(*size + *residuals_count, 0.0);
             let input = Constant::optimize(&data);
 
@@ -235,7 +242,6 @@ fn constant_compression_vs_residuals_benchmark(c: &mut Criterion) {
 
     group.finish();
 }
-
 
 // Define benchmark groups
 criterion_group!(
