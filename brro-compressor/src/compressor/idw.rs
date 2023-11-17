c792c70564bea819130842d9ac458cc3aee2f300
Carlos Rolo <3799585+cjrolo@users.noreply.github.com>
2023-11-17 12:25:34 +0000

    IDW without error control

--- brro-compressor/src/compressor/idw.rs
+++ brro-compressor/src/compressor/idw.rs
@@ -1,3 +1,5 @@
+use crate::utils::{DECIMAL_PRECISION, round_f64};
+
 /// IDW - Inverse Distance Weight
 /// This could be a subset of polynomial, or a replacement altogether
 /// The approach is very similar for both, but the methdo is different, and in some signals
@@ -48,24 +50,45 @@ impl Idw {
         self.max_position = pmax;
     }
 
+    fn locate_in_data_points(&self, point: f64) -> bool {
+        self.data_points.iter().any(|&i| i==point)
+    }
+
     // --- MANDATORY METHODS ---
     pub fn compress(&mut self, data: &[f64]) {
         if self.max_value == self.min_value { 
             debug!("Same max and min, we're done here!");
             return
-         }
-         // The algorithm is simple, Select 10% of the data points, calculate the IDW based on those data points
-         // Plus the max and min
-         let data_len = data.len();
-         // The minimum is a 3 point interpolation, otherwise, 1% of the data is used
-         let point_count = if 3 >= (data_len/100) { 3 } else { data_len/100 };
-         // I can calculate the positions from here
-         let points: Vec<f64> = (0..point_count).step_by(data_len/point_count).map(|f| f as f64).collect();
-         // I need to extract the values for those points
-         let values: Vec<f64> = points.iter().map(|&f| data[f as usize]).collect();
-         // Calculate IDW - I only need this on decompression...
-         // let idw = IDW::new(points, values);
-         self.data_points = values;
+        }
+        // The algorithm is simple, Select 10% of the data points, calculate the IDW based on those data points
+        // Plus the max and min
+        let data_len = data.len();
+        // The minimum is a 3 point interpolation, otherwise, 1% of the data is used
+        let point_count = if 3 >= (data_len/100) { 3 } else { data_len/100 };
+        // I can calculate the positions from here
+        let points: Vec<f64> = (0..data_len).step_by(data_len/point_count).map(|f| f as f64).collect();
+        // I need to extract the values for those points
+        let mut values: Vec<f64> = points.iter().map(|&f| data[f as usize]).collect();
+        
+        // I need to insert MIN and MAX only if they don't belong to the values already.
+        if self.min_position as f64 > *points.last().unwrap() {
+            // The position is behind the last point, so add it there
+            values.push(self.min_value as f64)
+        } else if !values.iter().any(|&i| i==self.min_value as f64) {
+            // Do the position exists already?
+            debug!("Min Inserted {} at index {}", self.min_value, self.min_position);
+            values.insert(self.min_position, self.min_value as f64);
+        }
+        if self.max_position as f64 > *points.last().unwrap() {
+            // The position is behind the last point, so add it there
+            values.push(self.max_value as f64)
+        } else if !values.iter().any(|&i| i==self.max_value as f64)  {
+            debug!("Max Inserted {} at index {}", self.max_value, self.max_position);
+            values.insert(self.max_position, self.max_value as f64);
+        }
+        debug!("Points: {:?}", points);
+        debug!("Values: {:?}", values);
+        self.data_points = values;
     }
 
     /// Decompresses data
@@ -80,20 +103,39 @@ impl Idw {
         bincode::encode_to_vec(self, config).unwrap()
     }
 
-    // TODO: Insert MIN and MAX
     pub fn to_data(&self, frame_size: usize) -> Vec<f64> {
         if self.max_value == self.min_value { 
             debug!("Same max and min, faster decompression!");
             return vec![self.max_value as f64; frame_size];
          }
-        let data_len = self.data_points.len();
-        // From the number of data points and the frame size, I can calculate the step and generate the points
-        let point_count = data_len;
-         // I can calculate the positions from here
-         let points: Vec<f64> = (0..point_count).step_by(data_len/point_count).map(|f| f as f64).collect();
-         let idw = IDW::new(points, self.data_points.clone());
-         // Build the data
-         (0..frame_size).map(|f| idw.evaluate(f as f64)).collect() 
+        //let data_len = self.data_points.len();
+        // How many points I should have
+        let point_count = if 3 >= (frame_size/100) { 3 } else { frame_size/100 };
+        // If they differ, it means I added either max and/or min
+        let point_dif = self.data_points.len() - point_count;
+        // I can calculate the positions from here
+        let mut points: Vec<f64> = (0..frame_size).step_by(frame_size/point_count).map(|f| f as f64).collect();
+        // Insert the position of min and max
+        debug!("Points diff: {}", point_dif);
+        if point_dif > 0 {
+            if self.min_position as f64 > *points.last().unwrap() {
+                // The position is behind the last point, so add it there
+                points.push(self.min_position as f64)
+            } else if !self.locate_in_data_points(self.min_value as f64) {
+                // Do the position exists already?
+                points.insert(self.min_position, self.min_position as f64);
+            }
+            if self.max_position as f64 > *points.last().unwrap() {
+                // The position is behind the last point, so add it there
+                points.push(self.max_position as f64)
+            } else if !self.locate_in_data_points(self.max_value as f64)  {
+                points.insert(self.max_position, self.max_position as f64);
+            }
+        }
+        debug!("{} {}", points.len(), self.data_points.len());
+        let idw = IDW::new(points, self.data_points.clone());
+        // Build the data
+        (0..frame_size).map(|f| round_f64(idw.evaluate(f as f64), DECIMAL_PRECISION)).collect() 
     }
 }
 
@@ -121,7 +163,7 @@ pub fn idw(data: &[f64]) -> Vec<u8> {
 pub fn idw_to_data(sample_number: usize, compressed_data: &[u8]) -> Vec<f64> {
     let c = Idw::decompress(compressed_data);
     c.to_data(sample_number)
-}
+} 
 
 #[cfg(test)]
 mod tests {
@@ -129,16 +171,25 @@ mod tests {
 
     #[test]
     fn test_idw() {
-        let vector1 = vec![1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 3.0, 1.0, 1.0, 5.0];
-        assert_eq!(idw(&vector1), [3, 1, 0, 0, 0, 0, 0, 0, 240, 63, 0, 0, 160, 64, 11, 0, 0, 128, 63, 0, 0]);
+        let vector1 = vec![1.0, 0.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 3.0, 1.0, 1.0, 5.0];
+        assert_eq!(idw(&vector1), [3, 5, 0, 0, 0, 0, 0, 0, 240, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 8, 64, 0, 0, 0, 0, 0, 0, 20, 64, 0, 0, 160, 64, 11, 0, 0, 0, 0, 1, 0]);
     }
 
     #[test]
     fn test_idw_compression() {
-        let vector1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 1.0, 1.0, 5.0, 1.0, 1.0, 5.0];
+        let vector1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 1.0, 1.0, 6.0, 0.0, 1.0, 9.0];
         let frame_size = vector1.len();
         let idw_data = idw(&vector1);
         let out = Idw::decompress(&idw_data).to_data(frame_size);
-        assert_eq!(out, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
+        assert_eq!(out, [1.0, 1.5279, 3.17159, 4.59747, 5.0, 4.79839, 4.62193, 4.94347, 6.0, 0.0, 4.65469, 9.0]);
+    }
+
+    #[test]
+    fn test_idw_linear_compression() {
+        let vector1 = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0];
+        let frame_size = vector1.len();
+        let idw_data = idw(&vector1);
+        let out = Idw::decompress(&idw_data).to_data(frame_size);
+        assert_eq!(out, [1.0, 1.62873, 3.51429, 4.84995, 5.0, 5.40622, 7.05871, 8.64807, 9.0, 9.37719, 11.18119, 12.0]);
     }
 }
--- brro-compressor/src/utils/mod.rs
+++ brro-compressor/src/utils/mod.rs
@@ -1,6 +1,9 @@
 pub mod error;
 pub mod writers;
 pub mod readers;
+
+pub const DECIMAL_PRECISION: u32 = 5;
+
 // Is this the right place?
 pub fn prev_power_of_two(n: usize) -> usize {
     // n = 0 gives highest_bit_set_idx = 0.
@@ -15,4 +18,16 @@ pub fn f64_to_u64(number: f64, precision: usize) -> u64 {
     if precision > 6 { panic!("Precision only available up to 6 digits!")}
     let mul = [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000][precision];
     (number * mul as f64) as u64
+}
+
+pub fn round_f32(x: f32, decimals: u32) -> f64 {
+    let y = 10i32.pow(decimals) as f64;
+    let out = (x as f64 * y).round() / y;
+    out
+}
+
+pub fn round_f64(x: f64, decimals: u32) -> f64 {
+    let y = 10i32.pow(decimals) as f64;
+    let out = (x * y).round() / y;
+    out
 }
